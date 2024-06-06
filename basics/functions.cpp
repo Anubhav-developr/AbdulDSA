5b92e0cd5046709b8424d6b741a179a4701fb9d2
Anubhav-developr <anubhavm1234@gmail.com>
2024-06-06 17:26:12 +0530

    some things

--- basics/functions.cpp
+++ basics/functions.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+//below fn have formal parameters
 int add(int a,int b){
     int c;
     c=a+b;
@@ -9,6 +10,7 @@ int main(){
 int x,y,z;
 x=5;
 y=10;
+//and here below is actual parameter
 z=add(x,y);
 cout<<z;
 
